81095da097b963e64666118937e309451a216c5c
kaulex <contact@kaulex.dev>
2022-03-31 22:03:53 +0200

    fixed broken structure

--- src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module javafxModule {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires tornadofx;
-    requires kotlin.stdlib;
-
-    exports com.example.demo.view;
-    exports com.example.demo.app;
-}
