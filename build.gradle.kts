8139149ee8ccc8db0a078a763d46344655cacd57
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-02-09 20:44:52 +0100

    WR-207: dependency update (#3)
    * WR-174: New auth endpoints
    
    * WR-207: Update to kotlin 1.8.0
    
    * WR-207: Update to ktor 2.2.3
    
    * WR-207: pdfbox to ktor 3.0.0-rc1
    
    * WR-207: kotlinx-serialization-json to ktor 1.4.1
    
    * WR-207: Remove ktlint as it only makes problems

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    val kotlinVersion = "1.8.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+    id("org.jetbrains.compose") version "1.3.0"
 }
 
 group = "org.datepollsystems.waiterrobot.mediator"
@@ -15,7 +14,6 @@ version = "1.0.0"
 repositories {
     google()
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
@@ -23,7 +21,7 @@ dependencies {
     implementation(compose.desktop.currentOs) // TODO how to build for multiple platforms
     implementation(compose.materialIconsExtended)
 
-    val ktorVersion = "2.0.3"
+    val ktorVersion = "2.2.3"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
@@ -32,9 +30,9 @@ dependencies {
     implementation("io.ktor:ktor-client-logging:$ktorVersion")
     implementation("io.ktor:ktor-client-websockets:$ktorVersion")
 
-    implementation("org.apache.pdfbox:pdfbox:3.0.0-alpha3")
+    implementation("org.apache.pdfbox:pdfbox:3.0.0-RC1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation(kotlin("reflect"))
 }
 
@@ -77,13 +75,4 @@ tasks.withType(Jar::class) {
 
 kotlin.sourceSets.all {
     languageSettings.optIn("kotlin.RequiresOptIn")
-}
-
-tasks.getByPath(":prepareKotlinBuildScriptModel").dependsOn("addKtlintFormatGitPreCommitHook")
-ktlint {
-    // Plugin currently uses ktlint version "0.43.2" but with "0.43.0" a bug was introduced that removes used wildcard imports.
-    // Bug was fixed with ktlint "0.45.0"
-    // -> Use the fixed ktlint version (0.46.0 introduces breaking changes)
-    // see https://github.com/pinterest/ktlint/issues/1256, https://github.com/pinterest/ktlint/issues/1277, https://github.com/pinterest/ktlint/issues/1393, https://github.com/pinterest/ktlint/pull/1402
-    version.set("0.45.2")
 }
