9a293e5278941c9e66f7f27c47f1a23799968a27
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-02-18 17:15:24 +0100

    WR-342: Use backend printer id for virtual printer path

--- src/main/kotlin/org/datepollsystems/waiterrobot/mediator/printer/VirtualLocalPrinter.kt
+++ src/main/kotlin/org/datepollsystems/waiterrobot/mediator/printer/VirtualLocalPrinter.kt
@@ -2,22 +2,24 @@ package org.datepollsystems.waiterrobot.mediator.printer
 
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.datepollsystems.waiterrobot.mediator.App
-import org.datepollsystems.waiterrobot.mediator.core.di.injectLogger
-import java.io.File
+import org.datepollsystems.waiterrobot.mediator.core.ID
+import org.datepollsystems.waiterrobot.mediator.core.di.injectLoggerForClass
 import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
 
-class VirtualLocalPrinter(number: Int) : AbstractLocalPrinter() {
-    override val localId: String = "virtualPrinter_$number"
-    override val name: String = "Virtual Printer $number"
-    private val basePath: File = Path(App.config.basePath, "virtualPrinters", number.toString()).toFile()
-    private val logger by injectLogger(this::class.simpleName + "#$localId")
+object VirtualLocalPrinter : AbstractLocalPrinter() {
+    override val localId: String = "virtualPrinter"
+    override val name: String = "Virtual Printer"
+    private val basePath: String = Path(App.config.basePath, "virtualPrinters").absolutePathString()
+    private val logger by injectLoggerForClass()
 
     init {
-        basePath.mkdirs()
-        logger.i("Will save to: ${basePath.absolutePath}")
+        logger.i("Will save to: $basePath")
     }
 
-    override fun printPdf(pdfId: String, document: PDDocument) {
-        document.save(File(basePath, "$pdfId.pdf"))
+    override fun printPdf(pdfId: String, bePrinterId: ID, document: PDDocument) {
+        val file = Path(basePath, bePrinterId.toString(), "$pdfId.pdf").toFile()
+        file.parentFile.mkdirs()
+        document.save(file)
     }
 }
