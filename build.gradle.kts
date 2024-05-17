949280dec5a02094ddc810a0cb253a92b23aaf07
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-05-17 17:25:37 +0200

    Fix koltin-compose compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.ByteArrayOutputStream
 
 plugins {
-    val kotlinVersion = "1.9.24"
+    val kotlinVersion = "1.9.23"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("org.jetbrains.compose") version "1.6.2"
