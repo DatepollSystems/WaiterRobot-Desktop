0e1f75dbfb8522b7eddee1d8ec081cad00281969
Fabian Schedler <dev.fabian.schedler@gmail.com>
2024-07-21 13:23:07 +0200

    feat: rounded icon on macOS

--- src/main/kotlin/org/datepollsystems/waiterrobot/mediator/ui/startUI.kt
+++ src/main/kotlin/org/datepollsystems/waiterrobot/mediator/ui/startUI.kt
@@ -1,7 +1,7 @@
 package org.datepollsystems.waiterrobot.mediator.ui
 
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -11,21 +11,12 @@ import androidx.compose.ui.window.rememberWindowState
 import org.datepollsystems.waiterrobot.mediator.core.ShortcutManager
 import org.datepollsystems.waiterrobot.mediator.navigation.Navigation
 import org.datepollsystems.waiterrobot.mediator.ui.theme.WaiterRobotTheme
-import java.nio.file.Paths
+import java.nio.file.Path
 import kotlin.io.path.exists
 import kotlin.io.path.inputStream
 
 fun startUI(onClose: () -> Unit = {}) {
     application {
-        val appIcon = remember {
-            System.getProperty("app.dir") // Only available when packaged
-                ?.let { Paths.get(it, "icon-512.png") }
-                ?.takeIf { it.exists() }
-                ?.inputStream()
-                ?.buffered()
-                ?.use { BitmapPainter(loadImageBitmap(it)) }
-        }
-
         Window(
             title = "kellner.team",
             icon = appIcon,
@@ -42,5 +33,21 @@ fun startUI(onClose: () -> Unit = {}) {
                 Navigation()
             }
         }
+    }
+}
+
+private val appIcon: Painter? by lazy {
+    // app.dir is set when packaged to point at our collected inputs.
+    val appDir = System.getProperty("app.dir")?.let { Path.of(it) }
+    // On Windows we should use the .ico file.
+    // On Linux, there's no native compound image format and Compose can't render SVG icons, so we pick the 128x128
+    // icon and let the frameworks/desktop environment rescale.
+    // On macOS we don't need to do anything.
+    var iconPath = appDir?.resolve("app.ico")?.takeIf { it.exists() }
+    iconPath = iconPath ?: appDir?.resolve("icon-square-128.png")?.takeIf { it.exists() }
+    if (iconPath?.exists() == true) {
+        BitmapPainter(iconPath.inputStream().buffered().use { loadImageBitmap(it) })
+    } else {
+        null
     }
 }
