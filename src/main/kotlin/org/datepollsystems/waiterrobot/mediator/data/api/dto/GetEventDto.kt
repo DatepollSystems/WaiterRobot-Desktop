190f42171162962248c24a118d693ee12ed81913
Fabian Schedler <fabian.schedler@studo.com>
2025-05-30 23:22:29 +0200

    Remove deprecated & unused property

--- src/main/kotlin/org/datepollsystems/waiterrobot/mediator/data/api/dto/GetEventDto.kt
+++ src/main/kotlin/org/datepollsystems/waiterrobot/mediator/data/api/dto/GetEventDto.kt
@@ -1,23 +1,16 @@
 package org.datepollsystems.waiterrobot.mediator.data.api.dto
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 import org.datepollsystems.waiterrobot.mediator.core.ID
-import java.time.LocalDate
 
 @Serializable
 data class GetEventDto(
     val id: ID,
     val name: String,
-    @SerialName("date") private val dateString: String? = null,
     val street: String,
     val streetNumber: String,
     val postalCode: String,
     val city: String,
     val organisationId: ID,
     val isDemo: Boolean
-) {
-    @Transient
-    val date: LocalDate? = dateString?.let { LocalDate.parse(it) }
-}
+)
